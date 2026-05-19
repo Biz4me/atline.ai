351187727a2834730bc9d1e8bc5db7f6bc37a296
Biz4me <125907060+Biz4me@users.noreply.github.com>
2026-05-19 08:59:53 +0200

    Remove PWA configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
-import withPWA from 'next-pwa'
 import { withPayload } from '@payloadcms/next'
 
-const nextConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-})({})
+const nextConfig = {}
 
 export default withPayload(nextConfig)
