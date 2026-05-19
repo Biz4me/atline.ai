70f7cb0823bffb646ecd76a3516b58d91ef7e79c
Biz4me <125907060+Biz4me@users.noreply.github.com>
2026-05-19 09:13:08 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
+tee ~/workspace/atline-v2/next.config.ts << 'ENDOFFILE'
 import type { NextConfig } from 'next'
 import { withPayload } from '@payloadcms/next'
 
 const nextConfig: NextConfig = {}
 
 export default withPayload(nextConfig)
+ENDOFFILE
+git add next.config.ts && git revert --continue --no-edit && git push origin main
